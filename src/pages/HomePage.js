3757373f7aa9ec65005ba9bdc1c881cd75f4784b
okabasele <ocekabasele@gmail.com>
2022-11-23 12:01:09 +0100

    bg image homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,10 +2,11 @@ import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Slider from "../components/Slider";
+import "../styles/HomePage.css";
 function HomePage({mangas}) {
   return (
     <>
-      <div className="home">
+      <div className="home bg-img">
         <Header />
         <Slider data={mangas}/>
         <Footer />
