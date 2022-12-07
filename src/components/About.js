020aeb85bf9d43681096b1a0ca8ee4e2a8fd1409
AndreClaveria <andreclaveria2002@gmail.com>
2022-12-07 14:40:47 +0100

    about

---
+++ src/components/About.js
@@ -0,0 +1,35 @@
+import "../styles/About.css"
+import AboutSlider from "./AboutSlider";
+import Slider from "./Slider";
+
+function About() {
+    return (
+        <>
+        <div className="about_text">
+            <h1>MangaPédie c'est quoi ?</h1>
+            <br></br>
+            <p>MangaPédie s’est créé sur un sujet et une problématique qui nous touche tous entre
+            membres de l’équipe.</p>
+
+            <p>Étant fans de Manga (Papier ou animation 2D), nous nous sommes rendus compte
+            d’une chose, au delà de lire ou regarder un manga et d’apprécier son histoire,
+            beaucoup de détails sont délaissés ou s’oublient facilement.</p>
+
+            <p>Nous savons que les auteurs n’écrivent pas une histoire par pur hasard, beaucoup
+            de manga ont des significations profondes et détails cachés ou très peu visibles
+            qu’on devrait prendre en compte. Nous pouvons citer par exemple les personnages
+            secondaires qui peuvent être beaucoup plus approfondis car ils sont souvent laissés
+            de côté, et pourtant ils ont tout de même un bon développement.</p>
+
+            <p>C’est pour cela qu’en tant que m’est venu l’idée principale, qui est de permettre aux
+            lecteurs et visionneurs de mangas/animés de pouvoir avoir accès facilement à
+            l’intégralité des informations qu’ils souhaitent, donc un panel complet d’informations
+            sur une sélection de mangas (exemples : résumés, biographie des personnages et
+            pas seulement principales, lieux importants, arcs, Galeries d’images, etc...).</p>
+        </div>
+
+        </>
+    )
+}
+
+export default About;
