5eedd8942e5fb837d3c36f78999e793645e80912
AndreClaveria <andreclaveria2002@gmail.com>
2022-12-07 09:41:01 +0100

    burger Menu + carrousel

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,11 +3,14 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Slider from "../components/Slider";
 import "../styles/HomePage.css";
+import Menu from "../components/Menu";
+
 function HomePage({mangas}) {
   return (
     <>
       <div className="home bg-img">
         <Header />
+        <Menu/>
         <Slider data={mangas}/>
         <Footer />
       </div>
